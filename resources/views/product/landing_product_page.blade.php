f8033a1755b6ee8313b67b910591f23a342c5937
daniil <daniil.koziura@altexsoft.com>
2018-05-25 19:44:52 +0300

    add Product structure

--- resources/views/product/landing_product_page.blade.php
+++ resources/views/product/landing_product_page.blade.php
@@ -19,107 +19,10 @@
 <div class="container">
     <div class="row">
 
-        <div class="card box-shadow col-md-3 card-block">
-            <img class="card-img-top" style=" padding:5px; height: 200px;" src="images/1.jpg">
+        <div class="card box-shadow col-md-3 card-block" >
+            <img class="card-img-top" style=" padding:5px; height: 200px;" src="{{asset('images/1.jpg')}}">
             <div class="card-body">
-                <p class="card-text">Lorem ipsum 16.</p>
-                <div class="d-flex justify-content-between align-items-center">
-                    <div class="btn-group">
-                        <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-                        <button type="button" class="btn btn-sm btn-outline-secondary">add +</button>
-                    </div>
-                    <small class="text-muted">9 mins</small>
-                </div>
-            </div>
-        </div>
-
-        <div class="card box-shadow col-md-3 card-block">
-            <img class="card-img-top" style=" padding:5px; height: 200px;" src="images/1.jpg">
-            <div class="card-body">
-                <p class="card-text">Lorem ipsum 16.</p>
-                <div class="d-flex justify-content-between align-items-center">
-                    <div class="btn-group">
-                        <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-                        <button type="button" class="btn btn-sm btn-outline-secondary">add +</button>
-                    </div>
-                    <small class="text-muted">9 mins</small>
-                </div>
-            </div>
-        </div>
-
-        <div class="card box-shadow col-md-3 card-block">
-            <img class="card-img-top" style=" padding:5px; height: 200px;" src="images/1.jpg">
-            <div class="card-body">
-                <p class="card-text">Lorem ipsum 16.</p>
-                <div class="d-flex justify-content-between align-items-center">
-                    <div class="btn-group">
-                        <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-                        <button type="button" class="btn btn-sm btn-outline-secondary">add +</button>
-                    </div>
-                    <small class="text-muted">9 mins</small>
-                </div>
-            </div>
-        </div>
-
-        <div class="card box-shadow col-md-3 card-block">
-            <img class="card-img-top" style=" padding:5px; height: 200px;" src="images/1.jpg">
-            <div class="card-body">
-                <p class="card-text">Lorem ipsum 16.</p>
-                <div class="d-flex justify-content-between align-items-center">
-                    <div class="btn-group">
-                        <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-                        <button type="button" class="btn btn-sm btn-outline-secondary">add +</button>
-                    </div>
-                    <small class="text-muted">9 mins</small>
-                </div>
-            </div>
-        </div>
-        <div class="card box-shadow col-md-3 card-block">
-            <img class="card-img-top" style=" padding:5px; height: 200px;" src="images/1.jpg">
-            <div class="card-body">
-                <p class="card-text">Lorem ipsum 16.</p>
-                <div class="d-flex justify-content-between align-items-center">
-                    <div class="btn-group">
-                        <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-                        <button type="button" class="btn btn-sm btn-outline-secondary">add +</button>
-                    </div>
-                    <small class="text-muted">9 mins</small>
-                </div>
-            </div>
-        </div>
-
-        <div class="card box-shadow col-md-3 card-block">
-            <img class="card-img-top" style=" padding:5px; height: 200px;" src="images/1.jpg">
-            <div class="card-body">
-                <p class="card-text">Lorem ipsum 16.</p>
-                <div class="d-flex justify-content-between align-items-center">
-                    <div class="btn-group">
-                        <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-                        <button type="button" class="btn btn-sm btn-outline-secondary">add +</button>
-                    </div>
-                    <small class="text-muted">9 mins</small>
-                </div>
-            </div>
-        </div>
-
-        <div class="card box-shadow col-md-3 card-block">
-            <img class="card-img-top" style=" padding:5px; height: 200px;" src="images/1.jpg">
-            <div class="card-body">
-                <p class="card-text">Lorem ipsum 16.</p>
-                <div class="d-flex justify-content-between align-items-center">
-                    <div class="btn-group">
-                        <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-                        <button type="button" class="btn btn-sm btn-outline-secondary">add +</button>
-                    </div>
-                    <small class="text-muted">9 mins</small>
-                </div>
-            </div>
-        </div>
-
-        <div class="card box-shadow col-md-3 card-block">
-            <img class="card-img-top" style=" padding:5px; height: 200px;" src="images/1.jpg">
-            <div class="card-body">
-                <p class="card-text">Lorem ipsum 16.</p>
+                <p class="card-text" style="font-size: 18px;">Lorem ipsum 1 9aaa.</p>
                 <div class="d-flex justify-content-between align-items-center">
                     <div class="btn-group">
                         <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
@@ -131,61 +34,6 @@
         </div>
 
 
-        <div class="card box-shadow col-md-3 card-block">
-            <img class="card-img-top" style=" padding:5px; height: 200px;" src="images/1.jpg">
-            <div class="card-body">
-                <p class="card-text">Lorem ipsum 16.</p>
-                <div class="d-flex justify-content-between align-items-center">
-                    <div class="btn-group">
-                        <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-                        <button type="button" class="btn btn-sm btn-outline-secondary">add +</button>
-                    </div>
-                    <small class="text-muted">9 mins</small>
-                </div>
-            </div>
-        </div>
-
-        <div class="card box-shadow col-md-3 card-block">
-            <img class="card-img-top" style=" padding:5px; height: 200px;" src="images/1.jpg">
-            <div class="card-body">
-                <p class="card-text">Lorem ipsum 16.</p>
-                <div class="d-flex justify-content-between align-items-center">
-                    <div class="btn-group">
-                        <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-                        <button type="button" class="btn btn-sm btn-outline-secondary">add +</button>
-                    </div>
-                    <small class="text-muted">9 mins</small>
-                </div>
-            </div>
-        </div>
-
-        <div class="card box-shadow col-md-3 card-block">
-            <img class="card-img-top" style=" padding:5px; height: 200px;" src="images/1.jpg">
-            <div class="card-body">
-                <p class="card-text">Lorem ipsum 16.</p>
-                <div class="d-flex justify-content-between align-items-center">
-                    <div class="btn-group">
-                        <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-                        <button type="button" class="btn btn-sm btn-outline-secondary">add +</button>
-                    </div>
-                    <small class="text-muted">9 mins</small>
-                </div>
-            </div>
-        </div>
-
-        <div class="card box-shadow col-md-3 card-block">
-            <img class="card-img-top" style=" padding:5px; height: 200px;" src="images/1.jpg">
-            <div class="card-body">
-                <p class="card-text">Lorem ipsum 16.</p>
-                <div class="d-flex justify-content-between align-items-center">
-                    <div class="btn-group">
-                        <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-                        <button type="button" class="btn btn-sm btn-outline-secondary">add +</button>
-                    </div>
-                    <small class="text-muted">9 mins</small>
-                </div>
-            </div>
-        </div>
 
     </div>
 </div>
